e6fcf38c507cfacfa4a8b9192dbb09952d3f7c61
Krish Chopra <krish.chopra@suno.com>
2025-12-22 16:47:01 -0500

    day 8 template

---
+++ krish/day-8/day-8-1.py
@@ -0,0 +1,4 @@
+lines = []
+with open("input-8.txt", "r") as f:
+    for line in f:
+        lines.append(line.strip())
---
+++ krish/day-8/day-8-2.py
@@ -0,0 +1,4 @@
+lines = []
+with open("input-7.txt", "r") as f:
+    for line in f:
+        lines.append(line.strip())
