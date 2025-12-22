0945417ce8ace7837330cb729db4819af757c288
parth <parth.chopra@berkeley.edu>
2025-12-22 09:21:44 -0500

    finish day 7

--- parth/day7/solution_pt_1.py
+++ parth/day7/solution_pt_1.py
@@ -1,5 +1,45 @@
+from typing import List
+
 with open('data.txt') as f:
     lines = [line.rstrip('\n') for line in f]
+    diagram = [[ch for ch in line] for line in lines]
 
-
+# iterative
+def calculate_beams_split(diagram: List[List[str]]) -> int:
+    rows, cols = len(diagram), len(diagram[0])
+    t, total_beams_split = 0, 0
+    # last time step, recursive base case
+    while t < rows:
+        if t == rows - 1:
+            return total_beams_split
+        beam_indexes = [i for i in range(cols) if diagram[t][i] == '|' or diagram[t][i] == 'S']
+        # advance forward
+        for idx in beam_indexes:
+            if diagram[t + 1][idx] == "^": # splitter
+                diagram[t + 1][max(0, idx - 1)] = "|"
+                diagram[t + 1][min(cols - 1, idx + 1)] = "|"
+                total_beams_split += 1
+            else:
+                diagram[t + 1][idx] = "|" # regular case
+        t += 1
     
+    return total_beams_split
+
+# recursive
+def advance_time_step(diagram: List[List[str]], t: int, total_beams_split: int = 0) -> int:
+    rows, cols = len(diagram), len(diagram[0])
+    # last time step, recursive base case
+    if t == rows - 1:
+        return total_beams_split
+    beam_indexes = [i for i in range(cols) if diagram[t][i] == '|' or diagram[t][i] == 'S']
+    # advance forward
+    for idx in beam_indexes:
+        if diagram[t + 1][idx] == "^": # splitter
+            diagram[t + 1][max(0, idx - 1)] = "|"
+            diagram[t + 1][min(cols - 1, idx + 1)] = "|"
+            total_beams_split += 1
+        else:
+            diagram[t + 1][idx] = "|" # regular case
+    return advance_time_step(diagram, t + 1, total_beams_split)
+
+print(calculate_beams_split(diagram))
--- parth/day7/solution_pt_2.py
+++ parth/day7/solution_pt_2.py
@@ -1,2 +1,58 @@
+from typing import List
+from collections import deque
+
 with open('data.txt') as f:
     lines = [line.rstrip('\n') for line in f]
+    diagram = [[ch for ch in line] for line in lines]
+
+# bfs, times out
+def calculate_total_timelines_bfs(diagram: List[List[str]]) -> int:
+    rows, cols = len(diagram), len(diagram[0])
+    initial_idx = [i for i in range(cols) if diagram[0][i] == 'S']
+    total_timelines = 0
+    queue = deque([(0, initial_idx[0])]) # tuple of (timestamp, idx)
+
+    while queue:
+        t, idx = queue.popleft()
+        # reached the end
+        if t == rows - 1:
+            total_timelines += 1
+            continue
+
+        if diagram[t + 1][idx] == "^":
+            if idx - 1 >= 0:
+                queue.append((t + 1, idx - 1)) # left timeline
+            if idx + 1 < cols:
+                queue.append((t + 1, idx + 1)) # right timeline
+        else:
+            queue.append((t + 1, idx))
+    
+    return total_timelines
+
+# dp approach
+def calculate_total_timelines_dp(diagram: List[List[str]]) -> int:
+    rows, cols = len(diagram), len(diagram[0])
+    initial_idx = [i for i in range(cols) if diagram[0][i] == 'S']
+    counts = [0 for _ in range(cols)] # similar to fibonacci approach
+    counts[initial_idx[0]] = 1
+
+    for r in range(rows - 1):
+        next_counts = [0 for _ in range(cols)]
+        next_row = diagram[r + 1]
+
+        for idx, num_ways in enumerate(counts):
+            if num_ways == 0:
+                continue
+            elif next_row[idx] == "^":
+                if idx > 0:
+                    next_counts[idx - 1] += num_ways
+                if idx + 1 < cols:
+                    next_counts[idx + 1] += num_ways
+            else:
+                next_counts[idx] += num_ways
+        
+        counts = next_counts
+    
+    return sum(counts)
+
+print(calculate_total_timelines_dp(diagram))
